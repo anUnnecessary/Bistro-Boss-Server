f238f00d9bf54bd9a6ec4af9472141ba170d107a
anUnnecessary <raihanshamil33@gmail.com>
2023-05-29 19:19:52 +0600

    jwt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
@@ -34,6 +35,14 @@ async function run() {
     const cartCollection = client.db("bistroDb").collection("carts");
 
 
+    app.post('/jwt', (req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET , { expiresIn: '1h' })
+
+      res.send({token})
+    })
+
+
 
     // users related api
 
@@ -53,7 +62,7 @@ async function run() {
       res.send(result);
     });
 
-    app.patch('users/admin/:id', async (req,res) => {
+    app.patch('/users/admin/:id', async (req,res) => {
       const id = req.params.id;
       const filter = {_id: ObjectId(id)};
       const updateDoc = {
