a2d5f7b8c1c53bc0674c79894c20665baad70057
anUnnecessary <raihanshamil33@gmail.com>
2023-05-29 19:37:35 +0600

    modified

--- index.js
+++ index.js
@@ -10,9 +10,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res , next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message: 'Unauthorized Access'});
+  }
+  // bearer token
+  const token = authorization.split('  ')[1];
 
-
-
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded )=> {
+    if(err){
+      return res.status(401).send({error: true, message: 'Unauthorized Access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8vqvwys.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -90,11 +103,16 @@ async function run() {
 
 
     // cart collection apis
-    app.get('/carts', async (req, res) => {
+    app.get('/carts', verifyJWT, async (req, res) => {
       const email = req.query.email;
 
       if (!email) {
         res.send([]);
+      }
+
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(403).send({error: true, message: 'Forbidden Access'})
       }
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
