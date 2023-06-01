dd57d18dcfc11c4b00433d2e0b79795334d8610a
anUnnecessary <raihanshamil33@gmail.com>
2023-06-01 14:29:57 +0600

    delete button added

--- index.js
+++ index.js
@@ -137,6 +137,15 @@ async function run() {
     })
 
 
+    app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    })
+    
+
+
     // review related apis
     app.get('/reviews', async (req, res) => {
       const result = await reviewCollection.find().toArray();
